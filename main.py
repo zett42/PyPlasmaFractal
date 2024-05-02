e81d897aa0027396b24a3735166528a5ea665c3a
zett42 <cerpendeel@gmx.net>
2024-05-03 01:10:01 +0200

    Refactor main script into a class

--- main.py
+++ main.py
@@ -36,194 +36,167 @@ import plasma_fractal_gui
 
 #---------------------------------------------------------------------------------------------------------------------------------------------
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+class PyPlasmaFractalApp:
 
-MY_APP_NAME = 'PyPlasmaFractal'
-MY_APP_AUTHOR = 'zett42'
+    def __init__(self):
+        """
+        Initializes logging and sets basic application constants.
+        """
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
+        self.app_name = 'PyPlasmaFractal'
+        self.app_author = 'zett42'
 
-def main():
-    """
-    Main function to initialize the environment, create the necessary objects,
-    and start the main rendering loop.
-    """
-    logging.debug('App started.')
 
-    render_config_manager, params = load_render_config()
+    def run(self):
+        """
+        Main entry point of the application which orchestrates the initialization,
+        main loop execution, and finalization.
+        """
+        logging.debug('App started.')
 
-    window, window_config_manager = initialize_glfw()
+        render_config_manager, self.params = self.load_render_config()
 
-    ctx = moderngl.create_context()
+        self.window, self.window_config_manager = self.initialize_glfw()
+        
+        self.ctx, self.feedback_manager = self.create_context_and_feedback_manager(self.window)
+        
+        self.im_gui_renderer = self.setup_imgui(self.window)
+        
+        self.main_loop()
+        
+        self.finalize_glfw()
+        
+        render_config_manager.save_config(self.params)
 
-    im_gui_renderer = setup_imgui(window)
 
-    main_loop(window, ctx, im_gui_renderer, params)
+    def main_loop(self):
+        """
+        Main rendering loop that continuously updates and displays the fractal visuals.
+        """
+        main_renderer = PlasmaFractalRenderer(self.ctx)
+        texture_to_screen = TextureRenderer(self.ctx)
+        timer = AnimationTimer()
 
-    finalize_glfw(window, window_config_manager)
+        while not glfw.window_should_close(self.window):
 
-    render_config_manager.save_config(params)
+            glfw.poll_events()
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
+            self.im_gui_renderer.process_inputs()
+            imgui.new_frame()
+            self.handle_gui()
 
-def main_loop(window, ctx, im_gui_renderer, params: PlasmaFractalParams):
-    """
-    Render loop that maintains the application's state, updates and renders
-    the scene, handles GUI interactions.
-    
-    Parameters:
-    - window: GLFW window object.
-    - ctx: ModernGL context for rendering.
-    - im_gui_renderer: ImGui GLFW renderer instance.
-    """
+            elapsed_time = timer.update(self.params.paused, self.params.speed)
 
-    main_renderer = PlasmaFractalRenderer(ctx)
+            main_renderer.update_params(self.params, self.feedback_manager.previous_texture, elapsed_time)
 
-    window_width, window_height = glfw.get_framebuffer_size(window)
+            self.feedback_manager.render_to_texture(main_renderer.current_vao)
 
-    # Using a floating point data type (f4) for the texture values is crucial for smooth feedback effect, which typically involves very small value changes.
-    feedback = FeedbackTextureManager(ctx, width=window_width, height=window_height, dtype='f4' 
-                                      ,filter_x=moderngl.LINEAR, filter_y=moderngl.LINEAR, 
-                                      repeat_x=True, repeat_y=True)
+            texture_to_screen.render(self.feedback_manager.current_texture)
 
-    texture_to_screen = TextureRenderer(ctx)
-    
-    timer = AnimationTimer()
+            imgui.render()
+            self.im_gui_renderer.render(imgui.get_draw_data())
 
-    while not glfw.window_should_close(window):
+            glfw.swap_buffers(self.window)
 
-        glfw.poll_events()  # Handle window events
-        im_gui_renderer.process_inputs()  # Process inputs for ImGui
-        imgui.new_frame()  # Start a new frame for ImGui
 
-        handle_gui(im_gui_renderer, params)
+    def load_render_config(self):
+        """
+        Loads or initializes the application's render configuration.
+        """
+        app_config_manager = ConfigFileManager(self.app_name, self.app_author, 'params.pkl')
+        params = app_config_manager.load_config() or PlasmaFractalParams(use_defaults=True)
 
-        elapsed_time = timer.update(params.paused, params.speed)
+        logging.debug("PlasmaFractalParams:\n" + '\n'.join(f"{key}={value}" for key, value in vars(params).items()))
 
-        main_renderer.update_params(params, feedback.previous_texture, elapsed_time)
+        return app_config_manager, params
 
-        # Let the main_renderer render to an offscreen texture
-        feedback.render_to_texture(main_renderer.current_vao)
 
-        # Copy the offscreen texture to the screen
-        texture_to_screen.render(feedback.current_texture)
+    def initialize_glfw(self):
+        """
+        Initializes GLFW for window and context management, loading configurations from WindowConfigManager.
+        """
+        if not glfw.init():
+            raise Exception("Failed to initialize GLFW")
+        
+        app_config_manager = WindowConfigManager(self.app_name, self.app_author)
 
-        # Render the ImGui elements to the screen
-        imgui.render()
-        im_gui_renderer.render(imgui.get_draw_data())
+        width, height, pos_x, pos_y = app_config_manager.get_config()
 
-        # Swap buffers for smooth display update
-        glfw.swap_buffers(window)  
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, glfw.TRUE)
+        glfw.window_hint(glfw.RESIZABLE, glfw.TRUE)
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
+        window = glfw.create_window(width, height, self.app_name, None, None)
+        if not window:
+            glfw.terminate()
+            raise Exception("Failed to create GLFW window")
 
-def load_render_config():
-    
-    app_config_manager = ConfigFileManager(MY_APP_NAME, MY_APP_AUTHOR, 'params.pkl')
+        glfw.make_context_current(window)
+        glfw.set_window_pos(window, pos_x, pos_y)
 
-    params = app_config_manager.load_config() or PlasmaFractalParams(use_defaults=True) 
-    #params = PlasmaFractalParams(use_defaults=True) 
+        return window, app_config_manager
 
-    logging.debug("PlasmaFractalParams:\n" + '\n'.join(f"    {key}={value}" for key, value in vars(params).items()))
 
-    return app_config_manager, params
+    def create_context_and_feedback_manager(self, window):
+        """
+        Creates and configures the ModernGL context and associated feedback texture manager.
+        """
+        ctx = moderngl.create_context()
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
+        window_width, window_height = glfw.get_framebuffer_size(window)
 
-def initialize_glfw():
-    """
-    Initializes GLFW and creates a window with an OpenGL context using configuration loaded by WindowConfigManager.
-       
-    Returns:
-    - A tuple containing the GLFW window object and the instance of WindowConfigManager or (None, None) if initialization fails.
-    """
-    if not glfw.init():
-        raise Exception("Failed to initialize GLFW")
+        feedback_manager = FeedbackTextureManager(ctx, width=window_width, height=window_height, dtype='f4', filter_x=moderngl.LINEAR, filter_y=moderngl.LINEAR, repeat_x=True, repeat_y=True)
 
-    app_config_manager = WindowConfigManager(MY_APP_NAME, MY_APP_AUTHOR)
-    width, height, pos_x, pos_y = app_config_manager.get_config()
+        return ctx, feedback_manager
 
-    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, glfw.TRUE)
 
-    window = glfw.create_window(width, height, MY_APP_NAME, None, None)
-    if not window:
+    def finalize_glfw(self):
+        """
+        Cleans up GLFW resources and saves the window configuration before exiting.
+        """
+        width, height = glfw.get_framebuffer_size(self.window)
+        pos_x, pos_y = glfw.get_window_pos(self.window)
+
+        self.window_config_manager.save_config(width, height, pos_x, pos_y)
+
         glfw.terminate()
-        raise Exception("Failed to create GLFW window")
 
-    glfw.make_context_current(window)
-    glfw.set_window_pos(window, pos_x, pos_y)
 
-    return window, app_config_manager
+    def handle_gui(self):
+        """
+        Handles the rendering and logic of the GUI elements with ImGui.
+        """
+        imgui.begin("Control Panel")
+        plasma_fractal_gui.handle_imgui_controls(self.params)
+        imgui.end()
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
 
-def finalize_glfw(window, app_config_manager):
-    """
-    Finalizes the GLFW context by saving the window configuration and terminating GLFW.
+    def setup_imgui(self, window):
+        """
+        Initializes and configures ImGui with GLFW.
+        """
+        imgui.create_context()
 
-    Parameters:
-    - window: The GLFW window object.
-    - app_config_manager: An instance of WindowConfigManager used to manage window configurations.
-    """
-    width, height = glfw.get_framebuffer_size(window)
-    pos_x, pos_y = glfw.get_window_pos(window)
-    app_config_manager.save_config(width, height, pos_x, pos_y)
-    glfw.terminate()
+        renderer = GlfwRenderer(window)
 
-#---------------------------------------------------------------------------------------------------------------------------------------------
+        # Replace default font with custom font
+        fonts = imgui.get_io().fonts
+        fonts.clear()
+        fontPath = os.path.join(os.path.dirname(__file__), 'fonts/Roboto-Regular.ttf')
+        fonts.add_font_from_file_ttf(fontPath, 20)
+        renderer.refresh_font_texture()
 
-def handle_gui(im_gui_renderer: GlfwRenderer, params: PlasmaFractalParams):
-    """
-    Renders the graphical user interface using ImGui and updates the GUI state.
+        return renderer
 
-    Parameters:
-    - im_gui_renderer: ImGui renderer for GLFW.
-    - params: An instance of GUIState that will be updated based on GUI interactions.
-    """
-    # Start a new ImGui window
-    imgui.begin("Control Panel")
-    
-    # Create the imgui controls and update the params attributes.
-    plasma_fractal_gui.handle_imgui_controls(params)
-    
-    # End the ImGui window
-    imgui.end()
-
-#---------------------------------------------------------------------------------------------------------------------------------------------
-
-def setup_imgui(window):
-    """
-    Sets up ImGui context and integrates it with GLFW.
-
-    Parameters:
-    - window: GLFW window object.
-
-    Returns:
-    - Initialized ImGui GLFW renderer.
-    """
-    imgui.create_context()
-
-    renderer = GlfwRenderer(window)
-
-    fonts = imgui.get_io().fonts
-    fonts.clear()  # to replace the default font
-
-    fontPath = os.path.join(os.path.dirname(__file__), 'fonts/Roboto-Regular.ttf')
-    fonts.add_font_from_file_ttf(fontPath, 20)
-    
-    renderer.refresh_font_texture()
-
-    return renderer
-
-#---------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
+    
     try:
-        main()
+        app = PyPlasmaFractalApp()  
+        app.run()
     except Exception as e:
         logging.error(f"Unhandled exception occurred: {e}", exc_info=True)
         raise
