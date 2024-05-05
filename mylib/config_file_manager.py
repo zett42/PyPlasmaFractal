562218c36251920aaebc4563dfedc987296d4605
zett42 <cerpendeel@gmx.net>
2024-05-05 02:19:53 +0200

    update ConfigFileManager comments and typing

--- mylib/config_file_manager.py
+++ mylib/config_file_manager.py
@@ -1,29 +1,47 @@
 import os
 import json
 import logging
+from typing import Any
 
 class ConfigFileManager:
     """
-    Manages the storage and retrieval of configuration objects in a serialized format using JSON.
-    
-    This class allows storing configurations in either user-specific or site-specific directories
-    determined by application details. It supports both default and custom serialization functions.
+    Manages the storage and retrieval of configuration data, facilitating operations such as saving and loading from a specified directory.
+
+    The class is designed to be flexible, accommodating different serialization formats beyond the default JSON. Users can provide custom 
+    functions for serialization and deserialization, allowing for handling of any data format that can be converted to and from a string.
+    This flexibility makes it suitable for a wide range of configuration management needs.
 
     Attributes:
-        app_name (str): The name of the application.
-        app_author (str): The author of the application.
-        default_filename (str): The default filename for storing the configuration file.
+        directory (str): The directory path where the configuration files are stored.
+        default_filename (str): The default filename used for storing and retrieving the configuration file.
+        save_function (callable): Function for serializing objects into strings. Defaults to a JSON serialization with indentation.
+        load_function (callable): Function for deserializing strings into objects. Defaults to standard JSON deserialization.
+
+    Methods:
+        save_config(config_object, filename=None): Saves the configuration object to a file using the specified or default filename.
+        load_config(filename=None, must_exist=False): Loads a configuration object from a file. Raises FileNotFoundError if
+                                                      the file does not exist and 'must_exist' is set to True.
     """
 
     def __init__(self, directory: str, filename='config.json', save_function=None, load_function=None):
         """
-        Initialize the ConfigFileManager object.
+        Initializes the ConfigFileManager with the necessary details for configuration file management.
+
+        This constructor sets up the manager to handle configuration files stored in the specified directory
+        and allows for using custom serialization functions. While the default serialization functions operate
+        on JSON, the class is designed to support any format that can be represented as a string, making it
+        versatile for various data handling needs.
 
         Args:
-            filename (str, optional): The name of the configuration file. Defaults to 'config.json'.
-            directory (str, optional): The full directory path where the configuration file will be stored. 
-            save_function (function, optional): A custom save function to be used instead of the default save function.
-            load_function (function, optional): A custom load function to be used instead of the default load function.
+            directory (str): The full directory path where the configuration files will be managed. This path
+                            should be provided by the user or determined by an external path management system.
+            filename (str, optional): The name of the configuration file. Defaults to 'config.json', which will be used
+                                    for saving and loading the configuration unless another filename is specified at the
+                                    time of calling save_config or load_config.
+            save_function (callable, optional): A function that takes a Python object and returns a string.
+                                                If not specified, a default function that formats JSON will be used.
+            load_function (callable, optional): A function that takes a string and returns a Python object.
+                                                If not specified, the standard JSON parsing method is used.
         """
         self.directory = directory
         self.default_filename = filename
@@ -33,18 +51,18 @@ class ConfigFileManager:
 
 
     @staticmethod
-    def _default_save(obj):
+    def _default_save(obj: Any):
         """ Default JSON save function. """
         return json.dumps(obj, indent=4)
 
 
     @staticmethod
-    def _default_load(json_str):
+    def _default_load(json_str: str):
         """ Default JSON load function. """
         return json.loads(json_str)
 
 
-    def save_config(self, config_object, filename=None):
+    def save_config(self, config_object: Any, filename:str=None):
         """
         Saves the configuration object to a file within the specified directory.
 
@@ -66,7 +84,7 @@ class ConfigFileManager:
             file.write(serialized_data)
 
 
-    def load_config(self, filename=None, must_exist=False):
+    def load_config(self, filename:str=None, must_exist:bool=False):
         """
         Loads a configuration object from a file in the specified directory.
 
